909fc852a2c7cff5588718be00e51b1292707148
shweta <shwetasinghh9416@gmail.com>
2024-05-04 14:48:48 +0530

    landing page added

---
+++ apps/shrink-app/components/Home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Image from "next/image";
+import heroimage from "../public/heroimage.png";
+
+const Home = () => {
+  return (
+    <div >
+      <Navbar />
+      <div className="flex flex-col items-center">
+
+      <Image src={heroimage} className="w-5/6" alt="h" />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,18 +1,17 @@
-"use client";
 
 import { ReactNode } from "react";
 
 interface ButtonProps {
   children: ReactNode;
   className?: string;
-  appName: string;
+  onClick: () => void;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ children, className, onClick }: ButtonProps) => {
   return (
     <button
       className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
+      onClick={onClick}
     >
       {children}
     </button>
